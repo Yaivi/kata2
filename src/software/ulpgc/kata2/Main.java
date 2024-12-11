a496bbddbd1b144d42722356fa7407b38c111bdd
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-12-11 19:58:10 +0000

    Main para comprobar funcionamiento, hecho, resultado correcto

--- src/software/ulpgc/kata2/Main.java
+++ src/software/ulpgc/kata2/Main.java
@@ -1,10 +1,20 @@
 package software.ulpgc.kata2;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        File file = new File("context.txt");
-
+    public static void main(String[] args) throws IOException {
+        File file = new File("content.txt");
+        List<Title> titles = new TsvFileTitleReader(file).readTitles();
+        HashMap<Integer, Integer> histogram = new HashMap<>();
+        for (Title title : titles) {
+            histogram.put(title.getStartYear(), histogram.getOrDefault(title.getStartYear(), 0) + 1);
+        }
+        for (Integer integer : histogram.keySet()) {
+            System.out.println(integer + ":" + histogram.get(integer));
+        }
     }
 }
